0a9cb131752eb69c34ec17d85a625f1864330b79
Humberto Diaz <hdkaratechop@gmail.com>
2022-05-14 21:39:50 -0700

    Add function to change cell background on clk

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let i=0, n=16;
     `<div class="row">${'<div class="cell"></div>'.repeat(n)}</div>`
     .repeat(n)
 
+// for each div in grid change background on hover
 
-
+cells = document.getElementsByClassName('cell');
+Array.from(cells).forEach(v => v.addEventListener('click', function() {
+    v.style.backgroundColor = 'red';
+  }));
 
