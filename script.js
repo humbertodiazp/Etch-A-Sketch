caebe1a3349f4a7ecba8a9aee12824b45da88628
Humberto Diaz <hdkaratechop@gmail.com>
2022-05-17 22:08:02 -0700

    Create function to create grid of any size

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
-let i=0, n=16;
+const cells = document.getElementsByClassName('cell');
+const restartButton = document.getElementById('reset')
+const arrayElements = [cells] 
 
- grid.innerHTML = 
-    `<div class="row">${'<div class="cell"></div>'.repeat(n)}</div>`
-    .repeat(n)
+
+
+function newGrid(x) {
+  let i=0, n=x;
+
+  grid.innerHTML = 
+  `<div class="row">${'<div class="cell"></div>'.repeat(n)}</div>`
+  .repeat(n)
+}
+newGrid(16);
+
+
 
 // for each div in grid change background on hover
 
-cells = document.getElementsByClassName('cell');
-Array.from(cells).forEach(v => v.addEventListener('click', function() {
-    v.style.backgroundColor = 'red';
+Array.from(cells).forEach(v => v.addEventListener('mouseover', function() {                    
+    v.style.backgroundColor = 'black';             
   }));
 
+                    
+Array.from(cells).forEach(v => v.addEventListener('mouseout', function() {
+  v.style.backgroundColor = 'black';
+}));
+
+
+restartButton.addEventListener('click', reset)
+
+// restartButton.addEventListener('click', clearBlock())
+
+function reset(){
+  forEach(arrayElements => {
+    arrayElements.style.backgroundColor ='gray';
+  })}
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                             
