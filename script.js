d706c49133a3a44911079ef9adb3d7361f32e6aa
Humberto Diaz <hdkaratechop@gmail.com>
2022-05-22 01:36:25 -0700

    Move mouseover/out listeners in func/leave outside
    Fixes no draw error.

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function newGrid(x) {
   grid.innerHTML = 
   `<div class="row">${'<div class="cell"></div>'.repeat(n)}</div>`
   .repeat(n)
-
+  //running this inside the function lends the hover effect
+  // to all instances of newGrid
   Array.from(cells).forEach(v => v.addEventListener('mouseover', function() {                    
     v.style.backgroundColor = 'black';             
   }));
@@ -21,38 +22,28 @@ function newGrid(x) {
   Array.from(cells).forEach(v => v.addEventListener('mouseout', function() {
   v.style.backgroundColor = 'black';
   }));
+  
 }
 
-newGrid();
+newGrid(25);
 
 
-
+//add event listeners to call newBoard function that will change cell size
 firstChoice.addEventListener('click', ()=> newBoard(25));
-
-
 secondChoice.addEventListener('click', ()=>newBoard(50) );
-
 thirdChoice.addEventListener('click', ()=>newBoard(75) );
 
+//add event listener to all cells to create hover effect
+Array.from(cells).forEach(v => v.addEventListener('mouseover', function() {                    
+  v.style.backgroundColor = 'black';             
+}));
 
+Array.from(cells).forEach(v => v.addEventListener('mouseout', function() {
+v.style.backgroundColor = 'black';
+}));
 
-// }
-// secondChoice.addEventListener('click', () => {
-//   newBoard = newGrid(50)
-//   for (const element of cells) {
-//     element.style.backgroundColor = '#b1acaa';
-//     element.style.width = '19.2px'; 
-//     element.style.height = '19.2px'; 
-// }
-
-// Choice.addEventListener('click', () => {
-//   newBoard = newGrid(50)
-//   for (const element of cells) {
-//     element.style.backgroundColor = '#b1acaa';
-//     element.style.width = '19.2px'; 
-//     element.style.height = '19.2px'; 
-// }
-
+//creates new grid based on event listener input with the correct cell size 
+//based on conditional relationships
 
 function newBoard(x){
   
@@ -73,7 +64,6 @@ function newBoard(x){
     }
 }
     
-
 
 
 // for each div in grid change background on hover
